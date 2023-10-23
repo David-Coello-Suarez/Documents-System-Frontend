644936cd0e163f09e672f95ae1a92d0290a8a6dc
SistemaYaneril <137332252+SistemaYaneril@users.noreply.github.com>
2023-10-23 10:36:25 -0500

    CORRECCIONES EN EL SUB MENU DE MULTIPLES NIVELES

--- src/components/iu/AppSidebar.tsx
+++ src/components/iu/AppSidebar.tsx
@@ -1,126 +1,91 @@
 import { useState } from "react"
 import { Link, useLocation } from "react-router-dom"
-import SimpleBar from "simplebar-react"
 import { useAppSelector } from "../../hooks"
-import "simplebar-react/dist/simplebar.min.css"
+import SimpleBar from "simplebar-react"
 import { isideba } from "../../interfaces"
 
 const AppSidebar = () => {
   const { sideba_sideba } = useAppSelector((state) => state.sideba)
 
   const path = useLocation().pathname
-
   return (
     <div className="sidebar" id="sidebar">
-      <div className="sidebar-inner slimscroll">
-        <SimpleBar className="sidebar-menu">
+      <SimpleBar className="sidebar-menu">
+        <div className="sidebar-inner slimscroll">
           <div id="sidebar-menu" className="sidebar-menu">
             <ul>
-              <li className="menu-title py-2">Principal</li>
+              <li className="menu-title">Principal</li>
               <li
                 className={`${path === "/" || path == "/dash" ? "active" : ""}`}
               >
-                <Link to={"."} className="py-2  px-2">
-                  <span
-                    className="material-symbols-outlined"
-                    style={{ lineHeight: "1", marginLeft: 0 }}
-                  >
-                    home
-                  </span>
-                  <span className="align-self-end">Dashboard</span>
+                <Link to={"."}>
+                  <i className="fa fa-dashboard" />
+                  <span>Dashboard</span>
                 </Link>
               </li>
               {sideba_sideba.map((sd) =>
                 sd.sideba_submen.length === 0 ? (
-                  <Pagina key={sd.sideba_sideba} propsp={sd} />
+                  <LinkPage key={sd.sideba_sideba} props={sd} />
                 ) : (
-                  <Submenu key={sd.sideba_sideba} props={sd} />
+                  <DropDownMenu props={sd} key={sd.sideba_sideba} />
                 ),
               )}
             </ul>
           </div>
-        </SimpleBar>
-      </div>
+        </div>
+      </SimpleBar>
     </div>
   )
 }
 
 export default AppSidebar
 
-interface isubmenu {
+interface idropdown {
   props: isideba
-  submenu?: boolean
 }
 
-const Submenu = ({ props, submenu }: isubmenu) => {
-  const [open, setOpen] = useState(false)
+const DropDownMenu = ({ props }: idropdown) => {
+  const [isSubmenuOpen, setSubmenuOpen] = useState(false)
 
-  if (props.sideba_submen.length == 0)
-    return <Pagina propsp={props} addClass submenu />
+  if (props.sideba_submen.length == 0) return <LinkPage props={props} />
 
-  const handleCollapse = () => setOpen(!open)
+  const handleClickMenu = (e: React.MouseEvent) => {
+    if (props.sideba_submen.length > 0) {
+      e.preventDefault()
+      setSubmenuOpen(!isSubmenuOpen)
+    }
+  }
 
   return (
-    <li className="submenu">
-      <a
-        onClick={handleCollapse}
-        className={`d-inline-flex justify-content-between m-0 py-2 w-100 ${
-          submenu ? (props.sideba_submen.length > 0 ? "ps-4" : "ps-5") : "px-2"
-        }`}
-      >
-        <div>
-          <i className="material-symbols-outlined">{props.sideba_sidico}</i>{" "}
-          <span className="ms-0"> {props.sideba_nombre} </span>
-        </div>
-        <span
-          className="material-symbols-outlined ms-1"
-          style={{ lineHeight: "1", marginRight: 0 }}
-        >
-          {!open ? "expand_more" : "expand_less"}
-        </span>
-      </a>
-      <ul className={` ${open ? "d-block" : "d-none"} `}>
-        {props.sideba_submen.length > 0 &&
-          props.sideba_submen.map((sdi) => (
-            <Submenu key={sdi.sideba_sideba} props={sdi} submenu />
-          ))}
-      </ul>
-    </li>
+    <>
+      <li className={`submenu ${isSubmenuOpen ? "open" : ""}`}>
+        <a onClick={handleClickMenu} className={isSubmenuOpen ? "subdrop" : ""}>
+          {props.sideba_sidico !== "" && <i className={props.sideba_sidico} />}
+          <span>{props.sideba_nombre}</span>
+          {props.sideba_submen.length > 0 && <span className="menu-arrow" />}
+        </a>
+
+        <ul style={{ display: isSubmenuOpen ? "block" : "none" }}>
+          {isSubmenuOpen &&
+            props.sideba_submen.length > 0 &&
+            props.sideba_submen.map((sdi) => (
+              <DropDownMenu props={sdi} key={sdi.sideba_sideba} />
+            ))}
+        </ul>
+      </li>
+    </>
   )
 }
 
-interface ipagina {
-  propsp: isideba
-  addClass?: boolean
-  submenu?: boolean
+interface ilinkpage {
+  props: isideba
 }
 
-const Pagina = ({ propsp, addClass, submenu }: ipagina) => {
-  const path = useLocation().pathname
-
-  const pathString = path.split("/").filter(Boolean)
-
-  return (
-    <li
-      style={{ cursor: "pointer" }}
-      className={`${pathString[1] === propsp.sideba_ventan ? "active" : ""}`}
-    >
-      <Link
-        to={propsp.sideba_ventan}
-        className={`${
-          addClass ? "align-items-center d-inline-flex w-100" : ""
-        } ${submenu ? "ps-5" : "px-2 py-2"}`}
-      >
-        <span
-          className="material-symbols-outlined"
-          style={{ lineHeight: "1", marginLeft: 0 }}
-        >
-          {propsp.sideba_sidico}
-        </span>
-        <span className={`${addClass ? "ms-2" : ""}`}>
-          {propsp.sideba_nombre}
-        </span>
-      </Link>
-    </li>
-  )
-}
+const LinkPage = ({ props }: ilinkpage) => (
+  <li>
+    <Link to={props.sideba_ventan}>
+      {props.sideba_sidico !== "" && <i className={props.sideba_sidico} />}
+      <span>{props.sideba_nombre}</span>
+    </Link>
+  </li>
+)
